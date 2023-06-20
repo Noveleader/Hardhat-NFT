916a60d32993c19f30363d8e0f31ab1ec04ad21f
Noveleader <jhaankush992@gmail.com>
2023-06-20 11:24:32 +0530

    Dynamic SVG contract update 🔥

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "base64-sol/base64.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract DynamicSvgNft is ERC721{
+contract DynamicSvgNft is ERC721 {
     // mint
     // store our SVG information somewhere
     // Some logic to say "SHOW X IMAGE" or "SHOW Y IMAGE"
@@ -12,18 +13,78 @@ contract DynamicSvgNft is ERC721{
     uint256 public s_tokenCounter;
     string private i_lowImageURI;
     string private i_highImageURI;
-    string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
-
-    constructor(string memory lowSvg, string memory highSvg) ERC721("DynamicSvgNft", "DSN") {
+    string private constant base64EncodedSvgPrefix =
+        "data:image/svg+xml;base64,";
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) public s_tokenIdToHighValue;
+    event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("DynamicSvgNft", "DSN") {
         s_tokenCounter = 0;
+        i_lowImageURI = svgToImageUri(lowSvg);
+        i_highImageURI = svgToImageUri(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
-    function svgToImageUri(string memory svg) public pure returns(string memory){
-        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-        return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+    function svgToImageUri(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return
+            string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
-    function mintNft() public {
+
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValue[s_tokenCounter] = highValue;
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
+        emit CreatedNFT(s_tokenCounter, highValue);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    //Overriding the tokenURI() function from ERC721
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = i_lowImageURI;
+        if (price >= s_tokenIdToHighValue[tokenId]) {
+            imageURI = i_highImageURI;
+        }
+
+        // string memory imageURI = "";
+        //We are just making our JSON object using concatenation - abi.encodePacked(arg);
+        //But we need to convert this JSON to bytes and then to base64
+        //We are using base64.encode(bytes) to convert to base64
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '", "description":"A NFT which changes based on the Chaibnlinke Feed",',
+                                '"attributes": [{"trait_type":"coolness","value":"100"}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
